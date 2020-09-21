9d8d608e7412cfec2d1f43d0345d6c6b5ceebcdd
reaksiyon1337 <37051115+reaksiyon@users.noreply.github.com>
2020-09-21 23:14:51 +0300

    Add files via upload

---
+++ Senchi/MRClient/MRClient/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MRClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.LightPink;
+            this.button1.Location = new System.Drawing.Point(327, 452);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(30, 459);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(295, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BAĞLANIYOR..";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(319, 0);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(281, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nick:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.HotPink;
+            this.textBox1.Location = new System.Drawing.Point(30, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 423);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(442, 514);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Senchi v2.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Senchi/MRClient/MRClient/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Telepathy;
+
+namespace MRClient
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = textBox3;
+            textBox3.Focus();
+        }
+        Telepathy.Client client = new Telepathy.Client();
+        Telepathy.Message msg;
+        //bool isFirstJoin = true;
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            client.Connect("88.245.212.22", 1337); // ip girilecek
+            textBox1.ReadOnly = true;
+           
+
+            System.Threading.Thread.Sleep(1000);
+            serverConnect();
+            
+
+           
+        }
+
+
+
+        public void serverConnect()
+        {
+
+
+            while (client.GetNextMessage(out msg))
+                    {
+
+                switch (msg.eventType)
+                        {
+                            case Telepathy.EventType.Connected:
+                                Console.WriteLine("Connected");
+                                label1.Text = "BAĞLI!";
+                                label1.ForeColor = Color.Snow;
+                                
+                        break;
+                            case Telepathy.EventType.Data: //msj alındığı yer
+                            
+                        string ascii = Encoding.ASCII.GetString(msg.data);
+                    
+                        if (!ascii.Contains("cls"))
+                            textBox1.Text += ascii + "\r\n";
+                        else
+                            textBox1.Text = "";
+                        System.Media.SoundPlayer player = new System.Media.SoundPlayer("sounds/nya.wav");
+                        player.Play();
+
+
+                        break;
+                            case Telepathy.EventType.Disconnected:
+                                Console.WriteLine("Disconnected");
+                                label1.Text = "BAĞLANTI HATASI!";
+                                label1.ForeColor = Color.Snow;
+                        break;
+                        }
+
+
+            }
+                    
+            
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        // send a message to server
+        //   client.Send(new byte[] { 0xFF });
+
+            if(textBox3.Text == "")
+            {
+                textBox1.Text += "Sevgili senchi üyemiz, lütfen önce isminizi giriniz :)\r\n";
+                return;
+            }
+         
+        string msg =  textBox3.Text.ToString() + ": " + textBox2.Text.ToString();
+            byte[] msgc = Encoding.Default.GetBytes(msg);
+
+            client.Send(msgc);
+            textBox2.Text = "";
+            serverConnect();
+            }
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          serverConnect();
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (textBox3.Text == "")
+                {
+                    textBox1.Text += "Sevgili senchi üyemiz, lütfen önce isminizi giriniz :)\r\n";
+                    return;
+                }
+
+                string msg = textBox3.Text.ToString() + ": " + textBox2.Text.ToString();
+                byte[] msgc = Encoding.Default.GetBytes(msg);
+
+                client.Send(msgc);
+                textBox2.Text = "";
+                serverConnect();
+                textBox2.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+
+            }
+        }
+    }
+}
